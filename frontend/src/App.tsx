cc3c35e021d592a084cfabf4da0cd452f1d4a9fb
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2023-01-17 01:18:46 +0900

    :construction:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,45 @@
-import { useQuery } from "@apollo/client";
-import { AppBar, Box, Container, CssBaseline, Toolbar } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useState } from "react";
-import { graphql } from "../src/gql";
-import "./App.css";
+// import { useQuery } from "@apollo/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+// import { graphql } from "../src/gql";
 
-const theme = createTheme();
+// const theme = createTheme();
 
-const allCourses = graphql(/* GraphQL */ `
-  query GetCourses {
-    schools {
-      studios {
-        timeSlots {
-          dayOfWeek
-          startTime
-          course {
-            id
-            name
-            price
-            instructor {
-              name
-            }
-          }
-        }
-      }
-    }
-  }
-`);
+// const allCourses = graphql(/* GraphQL */ `
+//   query GetCourses {
+//     schools {
+//       studios {
+//         timeSlots {
+//           dayOfWeek
+//           startTime
+//           course {
+//             id
+//             name
+//             price
+//             instructor {
+//               name
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `);
 
 function App() {
-  const { data } = useQuery(allCourses, { variables: {} });
-  const [count, setCount] = useState(0);
+  // const { loading, error, data } = useQuery(allCourses, { variables: {} });
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box>
-        <CssBaseline />
-        <AppBar position="static">
-          <Toolbar variant="dense"></Toolbar>
-        </AppBar>
-        <Container maxWidth="xl">
-          {/* {data && data.schools.map((school: any) => (
-            <div key={school.id}></div>
-          ))} */}
-        </Container>
-      </Box>
-    </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //   <Box>
+    //     <CssBaseline />
+    //     <AppBar position="static">
+    //       <Toolbar variant="dense"></Toolbar>
+    //     </AppBar>
+    //     <Container maxWidth="xl"></Container>
+    //   </Box>
+    // </ThemeProvider>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ frontend/src/pages/root.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@mui/material";
+
+export default function Root() {
+  return <Box>1</Box>;
+}
